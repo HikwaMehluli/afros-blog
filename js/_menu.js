74d22e0f9128cb5df83e915ebbae78f43084cc1e
Mehluli Hikwa <mehlulihikwa@gmail.com>
2021-08-05 10:46:36 +0200

    Add Menu

--- js/_menu.js
+++ js/_menu.js
@@ -1,11 +1,11 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("sideMenu").style.width = "250px";
+    // document.getElementById("main").style.marginLeft = "250px";
+    // document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
+    document.getElementById("sideMenu").style.width = "0";
+    // document.getElementById("main").style.marginLeft = "0";
+    // document.body.style.backgroundColor = "white";
 }
