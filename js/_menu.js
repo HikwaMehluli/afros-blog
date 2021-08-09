55be66ddcef4fc2559c46a66ed479a15c8dbee95
Mehluli Hikwa <mehlulihikwa@gmail.com>
2021-08-09 02:59:15 +0200

    Upgrade

--- js/_menu.js
+++ js/_menu.js
@@ -1,12 +1,11 @@
 function openNav() {
     document.getElementById("sideMenu").style.width = "250px";
     // document.getElementById("main").style.marginLeft = "250px";
-    // document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
+    // document.body.style.backgroundColor = "rgba(0,0,0,0.9)";
 }
 
 function closeNav() {
     document.getElementById("sideMenu").style.width = "0";
-    document.body;
     // document.getElementById("main").style.marginLeft = "0";
     // document.body.style.backgroundColor = "white";
 }
